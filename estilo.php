e1ac8010b13f849df64288de53a641f70ac5ebf6
jcalzateb <juankalzateb@hotmail.com>
2021-11-09 23:30:29 -0500

    Refresh

--- estilo.php
+++ estilo.php
@@ -1,7 +1,7 @@
 <style type="text/css">
 .portada{
     height: 100vh;
-    background-image: url("images/1.png");
+    background-image: url("images/1.jpg");
     background-size: cover;
     background-repeat:no-repeat;
     background-position: center center;
