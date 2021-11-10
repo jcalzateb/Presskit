9c85b80f9ba945749dbe088c97d33d5b9e4dd39a
jcalzateb <juankalzateb@hotmail.com>
2021-11-09 23:15:31 -0500

    Presskit

---
+++ estilo.php
@@ -0,0 +1,11 @@
+<style type="text/css">
+.portada{
+    height: 100vh;
+    background-image: url("images/1.png");
+    background-size: cover;
+    background-repeat:no-repeat;
+    background-position: center center;
+
+}
+</style>
+
